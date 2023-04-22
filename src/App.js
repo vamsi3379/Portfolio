206a0e4cf22616372be1802f79e8bcec3dcce8e8
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2023-04-22 15:46:05 -0400

    Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Preloader from '../src/components/Pre'
+import Home from './pages/Home.js'
+import About from './pages/About'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Preloader load={load} />
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/project' element={<Project />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
