72c872a9f45c86674ed5a2130e38073cbbcaa997
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2023-05-01 03:18:06 -0400

    Added functionality for Contact us

--- src/components/Contactpage/ContactForm.jsx
+++ src/components/Contactpage/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Form, Alert } from 'react-bootstrap';
 import emailjs from 'emailjs-com';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 
 const ContactForm = () => {
   const [name, setName] = useState('');
@@ -57,48 +57,53 @@ const ContactForm = () => {
         </div>
       </Form>
     </div> */}
+    {showSuccessMessage && (
+        <Alert variant="success" onClose={() => setShowSuccessMessage(false)} dismissible>
+          Message sent successfully!
+        </Alert>
+      )}
     <div id="contact" className="contact-area section-padding">
   <div className="container">
     <div className="section-title text-center">
-      <h1>Get in Touch</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae risus nec dui venenatis dignissim. Aenean vitae metus in augue pretium ultrices.</p>
+      <h1 style={{fontFamily:"aveneir next"}}>Get in Touch</h1>
+      <p style={{fontFamily:"aveneir next"}}>I'm always open to hearing about exciting new opportunities in the software engineering field. If you're interested in working with a driven and knowledgeable developer, don't hesitate to get in touch.</p>
     </div>
     <Row>
       <div className="col-lg-7">
         <div className="contact">
-          <form className="form" name="enq" method="post">
+        <Form onSubmit={handleSubmit}>
           <Row>
               <div className="form-group col-md-6">
-                <input type="text" name="name" className="form-control" placeholder="Name" required/>
+                <input style={{fontFamily:"aveneir next"}} type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} required />
               </div>
               <div className="form-group col-md-6">
-                <input type="email" name="email" className="form-control" placeholder="Email" required/>
+                <input style={{fontFamily:"aveneir next"}} type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
               </div>
               <div className="form-group col-md-12 contactmessage" style={{marginTop:"20px", marginBottom:"20px"}}>
-                <textarea rows="6" name="message" className="form-control" placeholder="Your Message" required></textarea>
+                <textarea style={{fontFamily:"aveneir next"}} as="textarea" rows={5} placeholder="Enter your message" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
               </div>
               <div className="col-md-12 text-center">
                 <button type="submit" value="Send message" name="submit" id="submitButton" className="btn btn-contact-bg" title="Submit Your Message!">Send Message</button>
               </div>
               </Row>
-          </form>
+          </Form>
         </div>
       </div>
       <div className="col-lg-5">
         <div className="single_address">
           <i className="fa fa-map-marker"></i>
-          <h4>Our Address</h4>
-          <p>3800 SW 34th Street Gainesville, FL 32608</p>
+          <h4 style={{fontFamily:"aveneir next"}}>Our Address</h4>
+          <p style={{fontFamily:"aveneir next"}}>3800 SW 34th Street Gainesville, FL 32608</p>
         </div>
         <div className="single_address">
           <i className="fa fa-envelope"></i>
-          <h4>Send your message</h4>
-          <p>vamsi.pachamatla@ufl.edu</p>
+          <h4 style={{fontFamily:"aveneir next"}}>Send your message</h4>
+          <p style={{fontFamily:"aveneir next"}}>vamsi.pachamatla@ufl.edu</p>
         </div>
         <div className="single_address">
           <i className="fa fa-phone"></i>
-          <h4>Call us on</h4>
-          <p>(+1) 352 740 9879</p>
+          <h4 style={{fontFamily:"aveneir next"}}>Call us on</h4>
+          <p style={{fontFamily:"aveneir next"}}>(+1) 352 740 9879</p>
         </div>
       </div>
       </Row>
@@ -107,11 +112,12 @@ const ContactForm = () => {
 <span></span>
 <span></span>
 <span></span>
-  <div class="container p-4"></div>
-  <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-    © 2020 Copyright:
-    <a class="text-white" href="https://vamsipachamatla.com">vamsipachamatla.com</a>
+  <div className="container p-4"></div>
+
+  <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)", fontFamily:"aveneir next" }}>
+   Thank You for visiting.
   </div>
+
 
     </>
   );
